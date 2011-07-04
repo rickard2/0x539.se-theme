582011db86b072d509152b3ec36da69560b31e1f
Rickard Andersson <rickard@montania.se>
2011-07-04 18:42:51 +0200

    modernizr feature detection flyttat font från import social

--- functions.php
+++ functions.php
@@ -133,6 +133,11 @@ function boilerplate_setup() {
 			'description' => __( 'Boilerplate', 'boilerplate' )
 		)
 	) );
+	
+	wp_enqueue_script( "modernizr", get_bloginfo('stylesheet_directory') . "/js/modernizr.js", array(), "2.0.6", false );
+	wp_enqueue_script( "0x539-se-script", get_bloginfo('stylesheet_directory') . "/js/script.js", array('jquery', 'modernizr'), "1.0", true );
+	
+	wp_enqueue_style( "indie-flower-font", ($_SERVER['HTTPS'] == "on" ? "https:" : "http:") . "//fonts.googleapis.com/css?family=Indie+Flower&v1", array(), "1", "all" );	
 }
 endif;
 
